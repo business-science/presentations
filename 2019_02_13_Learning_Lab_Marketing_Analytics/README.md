120acee985a735164b2a8514ebf51dc20801eb45
Matt Dancho <mdancho@gmail.com>
2019-02-13 14:02:35 -0500

    upload contents of Learning Lab #3

---
+++ 2019_02_13_Learning_Lab_Marketing_Analytics/README.md
@@ -0,0 +1,15 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# excel\_to\_r
+
+This repo includes the `code`, [slide deck](/Excel_To_R.pdf), and
+presentation materials for Business Science Learning Lab \#3: Marketing
+Analytics - Taking You Business Process from Excel to R and the
+Correlation Funnel.
+
+``` r
+knitr::include_graphics("Excel_to_R_thumb.jpg")
+```
+
+![](Excel_to_R_thumb.jpg)<!-- -->
---
+++ 2019_02_13_Learning_Lab_Marketing_Analytics/marketing_analysis.R
@@ -0,0 +1,126 @@
+# BUSINESS SCIENCE LEARNING LABS
+# EPISODE 3
+# MARKETING ANALYTICS: EXCEL TO R & THE CORRELATION FUNNEL
+
+
+# Libraries
+library(tidyverse)
+library(readxl)
+library(recipes)
+library(tidyquant)
+library(ggrepel)
+
+
+# 1.0 READ EXCEL SHEETS ----
+path   <- "data/bank_term_deposit_marketing_analysis.xlsx"
+sheets <- excel_sheets(path)
+
+
+# 2.0 INVESTIGATE DATA FOR EACH SHEET ----
+sheets %>%
+    map(~ read_excel(path  = path, sheet = .)) %>%
+    set_names(sheets)
+
+
+# 3.0 PERFORM VLOOKUP EQUIVALENT ----
+data_joined_tbl <- sheets[4:7] %>%
+    map(~ read_excel(path = path, sheet = .)) %>%
+    reduce(left_join)
+
+data_joined_tbl 
+
+
+# 4.0 ANALYZE ----
+
+# 4.1 Prepare Data ----
+# recipes reference: https://tidymodels.github.io/recipes/reference/index.html
+
+data_joined_tbl %>% glimpse()
+
+recipe_obj <- recipe(~ ., data = data_joined_tbl) %>%
+    step_rm(ID) %>%
+    step_discretize(all_numeric(), options = list(min_unique = 1)) %>%
+    step_dummy(all_nominal(), one_hot = TRUE, naming = partial(dummy_names, sep = "__")) %>%
+    prep()
+
+data_transformed_tbl <- data_joined_tbl %>%
+    bake(recipe_obj, new_data = .) 
+
+data_transformed_tbl %>% glimpse()
+
+# 4.2 Correlation Analysis ----
+
+# Prepare Correlations
+correlation_tbl <- data_transformed_tbl %>%
+    cor(y = data_transformed_tbl$TERM_DEPOSIT__yes) %>%
+    as_tibble(rownames = "feature") %>%
+    rename(TERM_DEPOSIT__yes = V1) %>%
+    separate(feature, into = c("feature", "bin"), sep = "__") %>%
+    filter(!is.na(TERM_DEPOSIT__yes)) %>%
+    filter(!str_detect(feature, "TERM_DEP")) %>%
+    arrange(abs(TERM_DEPOSIT__yes) %>% desc()) %>%
+    mutate(feature = as_factor(feature) %>% fct_rev())
+
+# Visualize Correlations
+correlation_tbl %>%
+    
+    ggplot(aes(TERM_DEPOSIT__yes, y = feature, text = bin)) +
+    
+    # Geometries
+    geom_vline(xintercept = 0, linetype = 2, color = "red") +
+    geom_point(color = "#2c3e50") +
+    geom_text_repel(aes(label = bin), size = 3, color = "#2c3e50") +
+    
+    # Formatting
+    expand_limits(x = c(-0.4, 0.4)) +
+    theme_tq() +
+    labs(title = "Bank Marketing Analysis",
+         subtitle = "Correlations to Enrollment in Term Deposit",
+         y = "", x = "Correlation to Term Deposit")
+
+
+
+# 4.3 Interpret Correlations ----
+
+# What are the bins?
+recipe_obj %>% tidy()
+
+bins_tbl <- recipe_obj %>% tidy(2)
+
+bins_tbl %>% filter(terms == "DURATION")
+
+
+
+# 5.0 STRATEGY ----
+# - FOCUS ON DURATION BIN4 and POUTCOME == success
+
+strategy_tbl <- data_joined_tbl %>%
+    select(DURATION, POUTCOME, TERM_DEPOSIT) %>%
+    mutate(POTENTIAL = case_when(
+        DURATION > 319 ~ "High Potential",
+        POUTCOME == "success" ~ "High Potential",
+        TRUE ~ "Normal"
+    )) %>%
+    group_by(POTENTIAL) %>%
+    count(TERM_DEPOSIT) %>%
+    mutate(prop = n / sum(n)) %>%
+    ungroup() %>%
+    mutate(label_text = str_glue("n: {n}
+                                 prop: {scales::percent(prop)}"))
+
+
+# 6.0 REPORT RESULTS ----
+
+strategy_tbl %>%
+    ggplot(aes(POTENTIAL, prop, fill = TERM_DEPOSIT)) + 
+    geom_col() +
+    geom_label(aes(label = label_text), fill = "white", color = "#2c3e50") +
+    scale_fill_tq() +
+    scale_y_continuous(labels = scales::percent_format()) +
+    theme_tq() +
+    labs(title = "Bank Marketing Strategy",
+         subtitle = str_glue("Targeting customers that haven't been contacted in 319 days 
+                             or those with prior enrollments yields 32% vs 4.3%")
+    )
+
+
